86d150323f51d5af4f981dad8eb0738c32f78b73
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2023-05-01 22:30:40 -0300

    first commit

---
+++ components/layout.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="mx-auto flex flex-col space-y-4 bg-main-blue">
+      <header className="container static top-0 z-40 bg-main-blue">
+        <div className="h-16  border-b-slate-200 py-4 flex items-center">
+          <nav className="ml-4 pl-6">
+          <Image src="/white-logo.svg"  alt='header-icon' width={150} height={500}/>
+          </nav>
+        </div>
+      </header>
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden bg-main-blue">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ utils/makechain.ts
@@ -0,0 +1,35 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const CONDENSE_PROMPT = `Dado a seguinte conversa e uma pergunta de acompanhamento, reformule a pergunta para que ela seja uma pergunta independente.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `Você é um assistente de IA útil. Use as seguintes informações de contexto para responder à pergunta no final. Se você não souber a resposta, apenas diga que não sabe. NÃO tente inventar uma resposta. Se a pergunta não estiver relacionada ao contexto, responda educadamente que você está ajustado apenas para responder a perguntas relacionadas ao contexto.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0, 
+    modelName: 'gpt-3.5-turbo', 
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: false, 
+    },
+  );
+  return chain;
+};
